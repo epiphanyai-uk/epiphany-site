d5cef2a418ebee3eaa1536130e2c9dcee6177d71
Milly Palmer <palmermilly9@gmail.com>
2026-05-17 16:48:01 +0100

    Update icons to svgs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,41 +25,64 @@ const Footer = () => {
                     </p>
 
                     <div className={styles.socialRow}>
-                        <a href="https://github.com/epiphanyai-uk" className={styles.socialIcon}>
-                            <i className="fab fa-github"></i></a>
-                        <a href="https://www.linkedin.com/company/epiphanyartificialintelligence/" className={styles.socialIcon}>
-                            <i className="fab fa-linkedin"></i></a>
+                    
+                        <ul>
+                            <li>
+                                <h2>Follow us here</h2>
+                                <a href="https://github.com/epiphanyai-uk" target="_blank" className={styles.socialIcon}>
+                                    <svg
+                                        className="button-icon"
+                                        role="presentation"
+                                        aria-hidden="true"
+                                    >
+                                        <use href="/icons.svg#github-icon"></use>
+                                    </svg>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/company/epiphanyartificialintelligence/" target="_blank" className={styles.socialIcon}>
+                                    <svg
+                                        className="button-icon"
+                                        role="presentation"
+                                        aria-hidden="true"
+                                    >
+                                        <use href="/icons.svg#discord-icon"></use>
+                                    </svg>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
+
+
+                    <div>
+                        <h3 className={styles.sectionTitle}>
+                            Company
+                        </h3>
+
+                        <ul className={styles.list}>
+                            <li>
+                                <Link to="/about" className={styles.link}>
+                                    About
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link to="/contact" className={styles.link}>
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+
+                    {/* Divider */}
+                    <div className={styles.bottom}>
+
+                        <p>© {new Date().getFullYear()} Epiphany Labs. All rights reserved.</p>
+
+                    </div>
+
                 </div>
-
-                <div>
-                    <h3 className={styles.sectionTitle}>
-                        Company
-                    </h3>
-
-                    <ul className={styles.list}>
-                        <li>
-                            <Link to="/about" className={styles.link}>
-                                About
-                            </Link>
-                        </li>
-
-                        <li>
-                            <Link to="/contact" className={styles.link}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-
-
-                {/* Divider */}
-                <div className={styles.bottom}>
-
-                    <p>© {new Date().getFullYear()} Epiphany Labs. All rights reserved.</p>
-
-                </div>
-
             </div>
 
 
